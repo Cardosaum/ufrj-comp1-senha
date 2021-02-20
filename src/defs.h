fa7ba6f0342d840e6452a0e2a7d4428b5f1a57a3
Albuquerque-David <davidra_rj@hotmail.com>
2021-02-20 20:11:08 -0300

    +added board size

--- src/defs.h
+++ src/defs.h
@@ -1,6 +1,10 @@
 #ifndef DEFS_H
 #define DEFS_H
 
+#include <stdbool.h>
+
+#define BOARD_SIZE 8
+
 typedef struct Password {
     int password[4];
 } Password;
@@ -8,10 +12,11 @@ typedef struct Password {
 typedef struct Guess {
     Password player_password;
     int feedback[4];
+    bool feedback_given;
 } Guess;
 
 typedef struct Board {
-    Guess rounds[8];
+    Guess rounds[BOARD_SIZE];
     Password password;
 } Board;
 
