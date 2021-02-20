893eb55649f74ed79fe4d447bd745e645502cb64
Cardosaum <cardosaum@pm.me>
2021-02-20 18:02:32 -0300

    replace Password struct
    Password strucs must be defined before struct Guess

--- src/defs.h
+++ src/defs.h
@@ -1,14 +1,14 @@
 #ifndef STRUCTURES_H
 #define STRUCTURES_H
 
+typedef struct Password {
+    int password[4];
+} Password;
+
 typedef struct Guess {
     Password player_password;
     int feedback[4];
 } Guess;
-
-typedef struct Password {
-    int password[4];
-} Password;
 
 typedef struct Board {
     Guess rounds[8];
